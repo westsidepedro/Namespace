ad399160624caf93f35cc17041133490079b132f
jpetterson <jpetterson@DS-17.decker.local>
2018-03-09 11:21:10 -0600

    3/9/18

--- src/Namespace.cpp
+++ src/Namespace.cpp
@@ -20,13 +20,13 @@ int main()
     cat1.speak();
 
     original::Cat cat2;	// tells it to use 'original namespace' doing it this way
-    cat2.speak();
+    cat2.speak();	// b/c we included the .h file above!
 
     nameAnything::Cat cat3;// tells it to use 'nameAnything namespace' doing it this way
-    cat3.speak();
+    cat3.speak();	   // b/c we included the .h file above!
 
-    cout << original::NAME << endl;
-    cout << nameAnything::NAME << endl;
+    cout << original::NAME << endl; //use 'NAME' from the 'original' namespace
+    cout << nameAnything::NAME << endl; //use 'NAME' from the 'nameAnything' namespace
 
     return 0;
     }
